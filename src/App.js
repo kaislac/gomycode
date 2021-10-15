4b5076b0140c15918bfe6f8d074bab11d41948bc
Kaislac <kaies_lachaal@yahoo.fr>
2021-10-15 17:41:04 +0100

    first Project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import './FormPage.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import FormPage1 from './FormPage1'
+
+function App() {
+  return <FormPage1 />
+}
+
+export default App
